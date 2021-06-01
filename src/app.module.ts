af960ef8b02d414a0d18761e205b7df67ce1579b
Ianka Karuzia <iankakaruzia@gmail.com>
2021-05-31 22:33:26 -0300

    feat: :lock: add security libs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,20 @@ import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { GraphQLModule } from '@nestjs/graphql'
 import { ConfigModule } from '@nestjs/config'
+import { ThrottlerModule } from '@nestjs/throttler'
 import { typeOrmConfig } from './config/typeorm.config'
 import { UsersModule } from './users/users.module'
 import { CryptographyModule } from './cryptography/cryptography.module'
 import { AuthModule } from './auth/auth.module'
 import { UploadModule } from './upload/upload.module'
-import { MailModule } from './mail/mail.module';
+import { MailModule } from './mail/mail.module'
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10
+    }),
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot(typeOrmConfig),
     GraphQLModule.forRoot({
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { graphqlUploadExpress } from 'graphql-upload'
+import * as helmet from 'helmet'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  app.use(helmet())
+
+  app.enableCors()
+
   app.useGlobalPipes(new ValidationPipe())
+
   app.use(graphqlUploadExpress({ maxFileSize: 1000000, maxFiles: 10 }))
+
   await app.listen(8080)
 }
 bootstrap()
